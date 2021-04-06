ae7e8bbecdf81ff394958cfe5e5425df3cdc7656
Alexis <alexis_ancona@hotmail.com>
2021-04-05 19:01:10 -0700

    29.- Listado de Post

--- applicaciones/blog/views.py
+++ applicaciones/blog/views.py
@@ -1,21 +1,33 @@
 from django.shortcuts import render
-
+from .models import Post, Categoria
 # Create your views here.
 
 def home(request):
-    return render(request, 'index.html')
+    posts = Post.objects.filter(estado = True)
+    ctx = {"posts": posts}
+    return render(request, 'index.html', ctx)
 
 def generales(request):
-    return render(request, 'generales.html')
+    posts = Post.objects.filter(estado = True, categoria = Categoria.objects.get(nombre = "General"))
+    ctx = {"posts": posts}
+    return render(request, 'generales.html', ctx)
 
 def programacion(request):
-    return render(request, 'programacion.html')
+    posts = Post.objects.filter(estado = True, categoria = Categoria.objects.get(nombre = "Programacion"))
+    ctx = {"posts": posts}
+    return render(request, 'programacion.html', ctx)
 
 def tutoriales(request):
-    return render(request, 'tutoriales.html')
+    posts = Post.objects.filter(estado = True, categoria = Categoria.objects.get(nombre = "Tutoriales"))
+    ctx = {"posts": posts}
+    return render(request, 'tutoriales.html', ctx)
 
 def tecnologia(request):
-    return render(request, 'tecnologia.html')
-    
+    posts = Post.objects.filter(estado = True, categoria = Categoria.objects.get(nombre = "Tecnologia"))
+    ctx = {"posts": posts}
+    return render(request, 'tecnologia.html', ctx)
+
 def videojuegos(request):
-    return render(request, 'videojuegos.html')
+    posts = Post.objects.filter(estado = True, categoria = Categoria.objects.get(nombre = "Videojuegos"))
+    ctx = {"posts": posts}
+    return render(request, 'videojuegos.html', ctx)
